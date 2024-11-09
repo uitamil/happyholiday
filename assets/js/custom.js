187a346ceafe204ed80d8f257fca73aa7b836328
Tamil <103590953+uitamil@users.noreply.github.com>
2024-11-09 14:00:28 +0530

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+
+	"use strict";
+
+    
+
+  
+
+    // 1. Scroll To Top 
+
+		$(window).on('scroll',function () {
+
+			if ($(this).scrollTop() > 600) {
+
+				$('.return-to-top').fadeIn();
+
+			} else {
+
+				$('.return-to-top').fadeOut();
+
+			}
+
+		});
+
+		$('.return-to-top').on('click',function(){
+
+				$('html, body').animate({
+
+				scrollTop: 0
+
+			}, 1500);
+
+			return false;
+
+		});
+ 
+
+
+   
+
+ 
+    // 6. Smooth Scroll spy
+        
+        $('.header-area').sticky({
+           topSpacing:0
+        });
+        
+        //=============
+
+        $('li.smooth-menu a').bind("click", function(event) {
+            event.preventDefault();
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - -1
+            }, 1200,'easeInOutExpo');
+        });
+        
+        $('body').scrollspy({
+            target:'.navbar-collapse',
+            offset:0
+        });
+
+    // 7.animation support
+
+        $(window).load(function(){
+
+            $(".about-us-txt h2").removeClass("animated fadeInUp").css({'opacity':'0'});
+            $(".about-us-txt button").removeClass("animated fadeInDown").css({'opacity':'0'});
+        });
+
+        $(window).load(function(){
+
+            $(".about-us-txt h2").addClass("animated fadeInUp").css({'opacity':'0'});
+            $(".about-us-txt button").addClass("animated fadeInDown").css({'opacity':'0'});
+
+        });
+            
+
+});	
+
+	
